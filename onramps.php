9b2d5ac4df87fa8de28623b4b307d1c553a89dad
Ramya krishna <ramyakrishnaimminni@gmail.com>
2024-06-20 20:07:09 +0530

    changes

--- onramps.php
+++ onramps.php
@@ -3,27 +3,31 @@
 add_filter( 'plugin_action_links', 'remove_delete_button', 10, 2 );
 add_action( 'bulk_actions-plugins', 'prevent_bulk_plugin_deletion' );
 
- // List of plugins to protect from deletion
-const protected_plugins = array(
-    'wp-plugin-hostgator/wp-plugin-hostgator.php',
-    'bluehost-wordpress-plugin/bluehost-wordpress-plugin.php' 
-);
-
 /**
  * Remove the delete button for brand plugin
+ *
+ * @param array  $actions An array of plugin action links
+ *
+ * @param string $plugin_file Path to the plugin
  */
 function remove_delete_button( $actions, $plugin_file ) {
-    if ( in_array( $plugin_file, protected_plugins ) ) {
-        unset( $actions['delete'] );
-    }
-    return $actions;
+	// List of plugins to protect from deletion
+	$protected_plugins = array(
+		'wp-plugin-hostgator/wp-plugin-hostgator.php',
+		'bluehost-wordpress-plugin/bluehost-wordpress-plugin.php',
+	);
+	if ( in_array( $plugin_file, $protected_plugins ) ) {
+		unset( $actions['delete'] );
+	}
+	return $actions;
 }
 
 /**
  * Restrict the bulk deletion of plugins
+ *
+ * @param array $actions An array of plugin action links
  */
 function prevent_bulk_plugin_deletion( $actions ) {
-    error_log(json_encode($actions));
-    unset( $actions['delete-selected'] );
-    return $actions;
+	unset( $actions['delete-selected'] );
+	return $actions;
 }
